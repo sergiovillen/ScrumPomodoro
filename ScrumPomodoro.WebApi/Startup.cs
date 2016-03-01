525c75b880868a6b43be6f60af6a30f902ad405d
Sergio <sergio.villen.galera@hotmail.fr>
2016-03-01 19:55:06 +0100

    Adding authorization provider

--- ScrumPomodoro.WebApi/Startup.cs
+++ ScrumPomodoro.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
 
 [assembly: OwinStartup(typeof(ScrumPomodoro.WebApi.Startup))]
@@ -13,10 +14,25 @@ namespace ScrumPomodoro.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            ConfigureOAuth(app);
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
             app.UseWebApi(config);
         }
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
 
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
     }
 }
