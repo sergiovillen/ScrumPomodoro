97b22c462b71e8cbc5df84f24667ec6c334efe31
Sergio <sergio.villen.galera@hotmail.fr>
2016-03-01 19:44:00 +0100

    Adding fake controller

---
+++ ScrumPomodoro.WebApi/Controllers/DashboardController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ScrumPomodoro.WebApi.Models;
+
+namespace ScrumPomodoro.WebApi.Controllers
+{
+    [RoutePrefix("api/dashboard")]
+    public class DashboardController : ApiController
+    {
+        [Authorize]
+        [Route("")]
+        public IHttpActionResult Get()
+        {
+            return Ok(new List<Pomodoro> { new Pomodoro { Title = "Implement your app."} });
+        }
+    }
+}
---
+++ ScrumPomodoro.WebApi/Models/Pomodoro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ScrumPomodoro.WebApi.Models
+{
+    public class Pomodoro
+    {
+        public string Title { get; set; }   
+    }
+}
